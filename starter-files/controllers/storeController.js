2c768f8a17d0bd6e371d53a8b7c39f990860fe31
FredrikFrostad <frostad.fredrik@gmail.com>
2019-02-22 21:34:28 +0100

    First commit after fork

---
+++ starter-files/controllers/storeController.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+	console.log(req.name);
+	res.render('index');
+};
+
+
+exports.addStore = (req, res) => {
+	res.render('editStore', {title: 'Add Store'});
+};
+
+ 
+exports.createStore = async (req, res) => {
+	const store = new Store(req.body);
+	await store.save();
+	req.flash('succes', `Successfully Created ${store.name}. Care to leave a rewiev?`);
+	res.redirect('/');
+};
+
+exports.getStores = async (req, res) => {
+	// 1. Query the database
+
+
+	res.render('stores', {title: 'Stores'});
+};
